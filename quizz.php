e5516183069233cb78cc8c127f03bf4a022da8e3
Ludovic2411 <ludovic.ceccotti@gmail.com>
2018-02-06 16:00:48 +0100

    first commit

---
+++ quizz.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Bonjour</h1>
+      <h2>Répond à ces questions</h2>
+      <form class="form" action="quizz-form.php" method="get">
+      <p>Pour quelle marque n'a pas couru Valentino Rossi?</p>
+      <input type="radio" name="marque" value="aprilia">aprilia <br>
+      <input type="radio" name="marque" value="KTM">KTM <br>
+      <p>Combien de titre a gagné Marc Marquez</p><br>
+      <input type="number" name="titres"> <br>
+      <input type="submit" name="submit" value="répondre">
+    </form>
+
+  </body>
+</html>
